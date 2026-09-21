98c32d6558922f57a7b8cc97f1c800ec7ef55f8b
HyunahGo <gusdkrh23@duksung.ac.kr>
2026-09-21 18:19:00 +0900

    fix: 헤더파일 인자 수정

--- include/event.h
+++ include/event.h
@@ -7,5 +7,5 @@
 #include "sensor/servo.h"
 
 void event_processing_thread(Ultrasonic& ultrasonic_1, Ultrasonic& ultrasonic_2, Camera& cam, SpectroscopySensor& spec, LedStrip& led, ServoManager& servo);
-void event_for_AI_Thread(Camera& cam, SpectroscopySensor& spec, LedStrip& led);
-void event_for_test_Thread(Camera& cam, SpectroscopySensor& spec, LedStrip& led);
+void event_for_AI_Thread(Camera& cam, SpectroscopySensor& spec, LedStrip& led, ServoManager& servo);
+void event_for_test_Thread(Camera& cam, SpectroscopySensor& spec, LedStrip& led, ServoManager& servo);
